b452920b3ce9b159b3dc1812b863f287d8f7f65a
lienepurmale <96015681+lienepurmale@users.noreply.github.com>
2022-10-24 12:30:02 +0200

    Update ParameterizedTests.java

--- src/test/java/junit5Tests/ParameterizedTests.java
+++ src/test/java/junit5Tests/ParameterizedTests.java
@@ -1,7 +1,16 @@
 package junit5Tests;
 
+import org.junit.jupiter.api.TestInstance;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.*;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.params.provider.Arguments.arguments;
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 
 public class ParameterizedTests {
 
@@ -45,14 +54,58 @@ public class ParameterizedTests {
     }
 
     @ParameterizedTest
-    @CsvFileSource(files = {"src/test/resources/Params/shoppinglist.csv", "src/test/resources/Params/shoppinglist2.csv"}, numLinesToSkip=1)
+    @CsvFileSource(files = {"src/test/resources/Params/shoppinglist.csv", "src/test/resources/Params/shoppinglist2.csv"}, numLinesToSkip = 1)
     void csvFileSource(String name, double price, int quantity, String unit, String provider) {
         System.out.println("name = " + name + ", price = " + price + ", quantity = " + quantity + ", unit = " + unit + ", provider = " + provider);
     }
 
     @ParameterizedTest
-    @CsvFileSource(files = {"src/test/resources/Params/shoppinglist3.csv"}, numLinesToSkip=1, delimiterString = "___")
+    @CsvFileSource(files = {"src/test/resources/Params/shoppinglist3.csv"}, numLinesToSkip = 1, delimiterString = "___")
     void csvFileSource2(String name, double price, int quantity, String unit, String provider) {
         System.out.println("name = " + name + ", price = " + price + ", quantity = " + quantity + ", unit = " + unit + ", provider = " + provider);
     }
+
+    @ParameterizedTest
+    @MethodSource(value = "sourceString")
+    void methodSource_String(String param1) {
+        System.out.println("param1 = " + param1);
+
+    }
+
+    List<String> sourceString() {
+        //processing done here
+        return Arrays.asList("tomato", "carrot", "cabbage");
+    }
+
+    @ParameterizedTest
+    @MethodSource(value = "sourceStringAsStream")
+    void methodSource_StringAsStream(String param1) {
+        System.out.println("param1 = " + param1);
+
+    }
+    Stream<String>sourceStringAsStream(){
+        return Stream.of("apple", "pear");
+    }
+
+    @ParameterizedTest
+    @MethodSource(value="sourceListStringDouble")
+    void methodSource_StringDoubleList(String param1, double param2){
+        System.out.println("param1 = " + param1 + ", param2 = " + param2);
+    }
+
+    List<Arguments>sourceListStringDouble(){
+        //processing done here
+        return Arrays.asList(arguments("tomato", 2.0), arguments("carrot", 3.6));
+    }
+
+    @ParameterizedTest
+    @MethodSource(value="sourceStream_StringDouble")
+    void methodSource_StringDoubleStream(String param1, double param2){
+        System.out.println("param1 = " + param1 + ", param2 = " + param2);
+    }
+
+    Stream<Arguments> sourceStream_StringDouble(){
+        return Stream.of(arguments("apple", 9.2), arguments("banana" , 2.7));
+    }
+
 }
