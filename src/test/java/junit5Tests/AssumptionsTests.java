dfa4a9643e94d6cdffa7dd033b05f5477e654d65
lienepurmale <96015681+lienepurmale@users.noreply.github.com>
2022-10-25 15:34:37 +0200

    Update AssumptionsTests.java

--- src/test/java/junit5Tests/AssumptionsTests.java
+++ src/test/java/junit5Tests/AssumptionsTests.java
@@ -12,7 +12,7 @@ public class AssumptionsTests {
     @ParameterizedTest(name = "Run: {index} - value: {arguments}")
     @ValueSource(ints = {1, 5, 8})
     void intValues(int theParam) {
-        assumeTrue(theParam > 4); //Alt + Enter for static import
+        assumeTrue(theParam > 4);               //Alt + Enter for static import
         System.out.println("theParam = " + theParam);
     }
 
@@ -21,5 +21,12 @@ public class AssumptionsTests {
     void csvSource_StringString(String param1, String param2) {
         assumeFalse(param1.equals("steve"), "The assumption failed for the following param2: " + param2);
         System.out.println("param1 = " + param1 + ", param2 = " + param2);
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"steve,5, true", "captain, 24, false", "bucky, 363, true"})
+    void csvSource_StringIntBoolean(String param1, int param2, boolean param3) {
+        assumingThat(param2 >20, () -> System.out.println("Code ran"));
+        System.out.println("param1 = " + param1 + ", param2 = " + param2 + ", param3 = " + param3);
     }
 }
